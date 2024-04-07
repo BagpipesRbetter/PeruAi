6090692be1800d486d62f8435a996af4c7a52257
Evan Barclay <119373672+BagpipesRbetter@users.noreply.github.com>
2024-04-07 14:12:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 #!/bin/python
 #change this to alter what model your using. ive had the best luck sticking with llama2 but others will work, see ollama documentation on what models are availble
 _model = "llama2"
+messages = []
 import ollama
 #downloads the correct llm model
-ollama.pull(_model)
+if _model != "llama2":
+    ollama.pull(_model)
+    
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from flaskwebgui import FlaskUI
+
 #initalizes the site and stream mechanisim
 app = Flask(__name__, static_folder='static')
+
 socketio = SocketIO(app, cors_allowed_origins="*")
+ui = FlaskUI(app, width=500, height=500) 
 #loads home page
 @app.route('/', methods=['GET'])
 def home(name=None):
+    
     return render_template('index.html', name=name)
 #handles generation
 @socketio.on('message')
@@ -22,13 +30,16 @@ def handle_message(data):
     for part in ollama.chat(model=_model , messages=[message], stream=True):
         print(part['message']['content'])
         socketio.emit('response', {'data': part['message']['content']})
-#loads about page
-@app.route('/about', methods=['GET'])
-def about():
-    return render_template('about.html')
-#starts everything up
+
 if __name__ == "__main__":
-    #change the port number to anything you want that your computer isnt already using.
-    app.run(host="0.0.0.0", port=5000)
+    #runs the app
+    FlaskUI(
+        app=app,
+        socketio=socketio,
+        server="flask_socketio",
+        port=5003,
+        width=800,
+        height=800,
+    ).run()
     
 
